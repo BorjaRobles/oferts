9ae2cf733754b8a6ed2a6a6c4088b25010d601a1
borjarobles <borjaruby@gmail.com>
2012-03-22 13:34:58 +0100

    slider de pagina principal version beta

--- app/assets/javascripts/efect.js
+++ app/assets/javascripts/efect.js
@@ -1,52 +1,51 @@
 $(document).ready(function(){
     
-    $("#bienvenido").hoverIntent({
-	 over: welcomeopen, 
-	 timeout: 500, 
-	 out: welcomeclose
-     });
+  $("#bienvenido").hoverIntent({
+    over: welcomeopen, 
+    timeout: 500, 
+    out: welcomeclose
+  });
     
-    $("#buscador").hoverIntent({
-	 over: buscadoropen, 
-	 timeout: 500, 
-	 out: buscadorclose
-     });
+  $("#buscador").hoverIntent({
+    over: buscadoropen, 
+    timeout: 500, 
+    out: buscadorclose
+  });
     
-        $("#curriculum").hoverIntent({
-	 over: curriopen, 
-	 timeout: 500, 
-	 out: curriclose
-     });
-    
-    
+  $("#curriculum").hoverIntent({
+    over: curriopen, 
+    timeout: 500, 
+    out: curriclose
+  });
 }); // close document.ready
 
 
 function curriopen(){
-     $("#curriculum_text").slideToggle('slow');
+  $("#curriculum_text").slideToggle('slow');
+  $("#curri_text_arrow").slideToggle('slow');
 }
+
 function curriclose(){
-     $("#curriculum_text").slideUp("slow");    
+  $("#curriculum_text").slideUp("fast");    
+  $("#curri_text_arrow").slideUp("fast");  
 }
 
 function buscadoropen(){
-     $("#buscador_text").slideToggle('slow');
+  $("#buscador_text").slideToggle('slow');
+  $("#busq_text_arrow").slideToggle('slow');
 }
+
 function buscadorclose(){
-     $("#buscador_text").slideUp("slow");    
+  $("#buscador_text").slideUp("fast");    
+  $("#busq_text_arrow").slideUp("fast");  
 }
      
 function welcomeopen(){
-     $("#welcome_text").slideToggle('slow');    
+  $("#welcome_text").slideToggle('slow');    
+  $("#welcome_text_arrow").slideToggle('slow');
 }
 
 function welcomeclose(){
-     $("#welcome_text").slideUp("slow");  
+  $("#welcome_text").slideUp("fast");  
+  $("#welcome_text_arrow").slideUp("fast");  
 }
-                
-                
-
-                
-    
-    
-   
